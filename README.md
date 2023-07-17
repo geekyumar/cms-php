b008b992f92e1158427794f603d3d3c8e8df5a4d
Umar Farooq <umar.great.01@gmail.com>
2023-07-17 18:39:36 +0000

    added README

---
+++ README.md
@@ -0,0 +1,16 @@
+## Content Management System (CMS)
+
+A Simple Content Management System (CMS) built with raw PHP and APIs.
+
+## Functionalities
+
+The Users can post: 
+	->Menu
+	->Pages
+	->Posts (with images)
+	->Products (with images)
+
+
+### This Project is Hosted on a live server. 
+[Click here](https://cms.umarfarooq.online/) for the link to app.
+
--- htdocs/users/signup.php
+++ htdocs/users/signup.php
@@ -16,7 +16,7 @@ if (session::get('session_token')) {
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Signup
+    Signup - CMS
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
